22317ed95742f5caeb5c84dcb44ba29b35fc9eda
White_Owl <desiderio.giovanni92@gmail.com>
2020-03-31 18:15:24 +0200

    Aggiunta classe Employee con proprietà

---
+++ Employees.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentNo19
+{
+    class Employees
+    {
+		private int employeeID;
+		private string firstName;
+		private string lastName;
+		private decimal salary;
+		private int age;
+		private List<int> appraisal;
+
+
+		public int EmployeeID
+		{
+			get { return employeeID; }
+			set { employeeID = value; }
+		}
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public List<int> Appraisal
+        {
+            get { return appraisal; }
+            set { appraisal = value; }
+        }
+
+    }
+}
